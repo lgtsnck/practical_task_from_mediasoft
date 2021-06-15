9b897f7885fd985fc3aa960eb8731f9e4f45dbc9
lgtsnck <aleksey.badanin777@gmail.com>
2021-06-15 21:55:29 +0400

    writing our views and urls for them

---
+++ movies/moviesapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+from .views import logout_user
+
+urlpatterns = [
+    path("", views.MoviesView.as_view(), name="movies_view"),
+    path("register/", views.RegisterUser.as_view(), name="register"),
+    path("login/", views.LoginUser.as_view(), name="login"),
+    path("logout/", logout_user, name="logout"),
+    path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
+]
--- movies/moviesapp/views.py
+++ movies/moviesapp/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.contrib.auth import logout, login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from .models import Movie
+
 
 # Create your views here.
+
+class MoviesView(ListView):
+    """Список фильмов"""
+    model = Movie
+    queryset = Movie.objects.all()
+
+
+class MovieDetailView(DetailView):
+    """Детали фильма"""
+    model = Movie
+    slug_field = "url"
+
+
+class RegisterUser(CreateView):
+    form_class = UserCreationForm
+    template_name = "moviesapp/register.html"
+    success_url = reverse_lazy("movies_view")
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect("movies_view")
+
+
+class LoginUser(LoginView):
+    form_class = AuthenticationForm
+    template_name = "moviesapp/login.html"
+
+    def get_success_url(self):
+        return reverse_lazy("movies_view")
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("movies_view")
